126094d6bdfd2185743e0967a582c0c701f20a9d
shouchao.shi <stephenAlan@163.com>
2021-02-09 15:03:04 +0800

    redis,shardingjdbc

---
+++ spring-boot-redis/src/main/java/com/stephen/demo/Main.java
@@ -0,0 +1,16 @@
+package com.stephen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by stephen on 2021-02-01 18:29 .
+ * Description:
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
